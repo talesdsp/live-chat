cb7751be7a26849a26084ac86abd30b6076a507b
Tales Pereira <talesdsp@gmail.com>
2020-02-14 16:43:33 -0300

    added underscorejs

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require("express");
-const socket = require("socket.io");
-
+const path = require("path");
 const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "public"));
 
-app.listen(4000, () => console.log("listening on port 4000"));
+io.on("connection", function(socket) {
+  console.log("made socket connection", socket.id);
+
+  socket.on("submit", function(data) {
+    io.sockets.emit("submit", data);
+  });
+
+  socket.on("type", function(data) {
+    socket.broadcast.emit("type", data);
+  });
+});
+
+server.listen(4000);
