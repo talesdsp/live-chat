37e37be412ad75fc9fa7238729f9bf41f4f88a0b
Tales Pereira <talesdsp@gmail.com>
2020-02-14 16:43:02 -0300

    fix infinite typing

---
+++ public/chat.js
@@ -0,0 +1,32 @@
+const socket = io.connect("http://localhost:4000");
+
+const message = document.querySelector("#message");
+const author = document.querySelector("#author");
+const btn = document.querySelector("#send");
+const output = document.querySelector("#output");
+const feedback = document.querySelector("#feedback");
+
+//  EMITTERS
+btn.addEventListener("click", () => {
+  socket.emit("submit", {
+    message: message.value,
+    author: author.value
+  });
+});
+
+message.addEventListener("keypress", () => {
+  socket.emit("type", {author: author.value, message: message.value});
+});
+
+// LISTENERS
+socket.on("submit", (data) => {
+  feedback.innerHTML = "";
+  output.innerHTML += `<p><strong>${data.author}</strong> ${data.message}</p>`;
+});
+
+const stop = _.debounce(() => (feedback.innerHTML = ""), 2000);
+
+socket.on("type", (data) => {
+  feedback.innerHTML = `<p><em>${data.author} is typing a message...</em></p>`;
+  lazyType();
+});
